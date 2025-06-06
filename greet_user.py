752cbc6f42b7e99f1f58157d7fc4f3f0473b365c
Nicki Josh <nicholasjoshua26@gmail.com>
2025-06-06 17:54:15 +0100

    Added greet_.py script

---
+++ greet_user.py
@@ -0,0 +1,12 @@
+# greet_user.py
+
+# Step 1: Define a function called greet that takes a parameter 'name'
+def greet(name):
+    #""""This function prints a greeting message to the user."""
+    print(f"Hello, {name}! Welcome to python practice.")
+
+# Step 2: Ask the user to enter their name
+user_name = input("Enter your name: ")
+
+# Step 3: Call the greet function with the user's input
+greet(user_name)
